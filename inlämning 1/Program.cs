b3ffa3a6c5064e87b0d1c6ac0928ff05f0fe235d
absa07002 <absa07002@DESKTOP-TAGHD5A>
2024-10-03 13:49:49 +0200

    Add project files.

---
+++ inlämning 1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Uppgift_1 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilket favorit bok har du?");
+            string boknamn=Console.ReadLine();
+            Console.WriteLine("jag har kylan");
+            Console.WriteLine("kylan");
+        }
+    }
+}
